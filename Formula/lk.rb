cb90d7a0a6c51c54148eef4a010010a70d505d43
syarihu <info@syarihu.net>
2026-07-03 12:19:53 +0900

    chore: update lk to v0.13.1

--- Formula/lk.rb
+++ Formula/lk.rb
@@ -1,26 +1,26 @@
 class Lk < Formula
   desc "Local knowledge base CLI for Claude Code"
   homepage "https://github.com/syarihu/local-knowledge-cli"
-  version "0.13.0"
+  version "0.13.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/syarihu/local-knowledge-cli/releases/download/v#{version}/lk-aarch64-apple-darwin.tar.gz"
-      sha256 "2e7a94c51e4f82db328231a040be1da670a73c9768929ea359a08839bc3f057a"
+      sha256 "f323916e29fb4a80fbbaee545fdd3f7633252ae3a7c9bb48d9950d9d862f61c3"
     else
       url "https://github.com/syarihu/local-knowledge-cli/releases/download/v#{version}/lk-x86_64-apple-darwin.tar.gz"
-      sha256 "24e06e58c8e5e5bc88dcf234b78b626243e25e71dad166aba1836fb5b203ef0f"
+      sha256 "ff83fc06173cd2cf5273c79cbd244e5231066be2b251d15224f288239aea21c6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/syarihu/local-knowledge-cli/releases/download/v#{version}/lk-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0bcf29b5ab0d7639caa82dc20a69ba6236cffbaed1632370a9be431548e6fa0e"
+      sha256 "970bc60bc2237b93483f60a33c62afa6fd5f366f4bc0f48ecd4cc6517c2be774"
     else
       url "https://github.com/syarihu/local-knowledge-cli/releases/download/v#{version}/lk-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8cce5b9997a233cb9aec6c5a878000e6f443864d3c1efa959697d947518171f5"
+      sha256 "85fbc13ad43a7a6624239e501a4678f6a4f2cc601239d0b6a321c2fe492cb98b"
     end
   end
 
