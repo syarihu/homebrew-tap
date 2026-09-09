c87edaacbcf2a8080338e3758e00538ae0099838
Taichi Sato <info@syarihu.net>
2026-09-10 06:43:02 +0900

    Update agent-adjutant to v0.1.1

--- Formula/agent-adjutant.rb
+++ Formula/agent-adjutant.rb
@@ -5,21 +5,21 @@ class AgentAdjutant < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.0/agent-adjutant-aarch64-apple-darwin.tar.gz"
-      sha256 "6edc97bdfdf130b504bfc718e73ab862cb559126dc5bc626729a010d5e474470"
+      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.1/agent-adjutant-aarch64-apple-darwin.tar.gz"
+      sha256 "30fe8f023c8f8f1b3db38d7a418b9fe4250f5bd5833d6f808b3cfb8cdd08938c"
     else
-      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.0/agent-adjutant-x86_64-apple-darwin.tar.gz"
-      sha256 "d719a925d4ef2368c0e8c61c746ccb93e2167c1e5fcf5636245c6aa32af7cc56"
+      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.1/agent-adjutant-x86_64-apple-darwin.tar.gz"
+      sha256 "814b20d393beb6c8495d27f619a87a8ba3da4ca8fae329b8ac9d448acb9eab4a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.0/agent-adjutant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "1829dcb78fc3b3d69252c766f9df0b5c14de46ec1f0ec41b62ffa6f1045051f7"
+      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.1/agent-adjutant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "416adb4396862bddb533e1d1f8f9ed293ecaeeeff6ad99afbf846e7458b7a730"
     else
-      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.0/agent-adjutant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "08390b7a68a1152d025f29100944d43b9c3cb749bb0545bb7256c2df14b9c2da"
+      url "https://github.com/syarihu/agent-adjutant/releases/download/v0.1.1/agent-adjutant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "03eaeebc11ad55cb40565d7dc701a30ffc53a0609cb68644b87b9ddd402ac500"
     end
   end
 
@@ -29,7 +29,7 @@ class AgentAdjutant < Formula
   end
 
   test do
-    assert_match "adjutant 0.1.0", shell_output("#{bin}/adjutant --version")
-    assert_match "adjutant 0.1.0", shell_output("#{bin}/adj --version")
+    assert_match "adjutant 0.1.1", shell_output("#{bin}/adjutant --version")
+    assert_match "adjutant 0.1.1", shell_output("#{bin}/adj --version")
   end
 end
